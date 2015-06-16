ed47b5411c548b54e65469f81222f1399dcf72a9
Ellen <galperie@lafayette.edu>
2015-06-16 14:08:03 -0500

    add Book class

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by egalperi on 6/16/15.
+ */
+public class Book {
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
